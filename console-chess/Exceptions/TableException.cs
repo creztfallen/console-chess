35273984f33e37677ede58aca0c4936d1bd7583d
Mateus Moreno <mtssmoreno@gmail.com>
2023-07-28 17:49:55 -0300

    add validation and exception treatment

---
+++ console-chess/Exceptions/TableException.cs
@@ -0,0 +1,10 @@
+﻿namespace console_chess.Exceptions
+{
+    class TableException : Exception
+    {
+        public TableException(string message) : base(message) 
+        {
+  
+        }
+    }
+}
--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,11 +1,18 @@
 ﻿using Chess;
 using console_chess;
+using console_chess.Exceptions;
 using Table;
 
-GameTable tab = new GameTable(8, 8);
+try
+{
+    GameTable tab = new GameTable(8, 8);
 
-tab.PlaceThePiece(new Position(0, 0), new Knight(tab, Color.Black));
-tab.PlaceThePiece(new Position(1, 2), new Queen(tab, Color.Black));
-tab.PlaceThePiece(new Position(2, 3), new Rook(tab, Color.Black));
+    tab.PlaceThePiece(new Position(0, 0), new Knight(tab, Color.Black));
+    tab.PlaceThePiece(new Position(0, 0), new Queen(tab, Color.Black));
+    tab.PlaceThePiece(new Position(2, 3), new Rook(tab, Color.Black));
 
-Screen.PrintTable(tab);
+    Screen.PrintTable(tab);
+} catch (TableException ex)
+{
+    Console.WriteLine(ex.Message);
+}

aa3d92ec1f80fae10fa2ffcc8482f42f5f1b882b
Mateus Moreno <mtssmoreno@gmail.com>
2023-07-28 11:49:22 -0300

    add position class

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Table;
+
+Position position = new Position(2, 4);
+
+Console.WriteLine(position);
